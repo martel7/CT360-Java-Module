6599d49011871f2d05c7866be998957f799c6ae9
Dusan D Markovic <Dusan-D.Markovic@comtrade.com>
2022-09-19 14:49:55 +0200

    Finished the BALLOON assignment from day1

---
+++ JavaBaloon/src/main/java/org/example/Solution.java
@@ -0,0 +1,41 @@
+package org.example;
+
+public class Solution {
+
+    public int solution (String S){
+
+        //Base case, there is no Balloons in a string with less than 7 characters
+        if(S.length() < 7) return 0;
+
+        int noOfBalloons = 0;
+        String balloon = "BALLOON";
+        String findBalloon = "";
+        String replace, addChar;
+
+        for(int i = 0; i < 7; i++){
+
+
+            if(S.indexOf(balloon.charAt(i)) >= 0){
+                //System.out.println(S.indexOf(balloon.charAt(i)));
+                replace = "";
+                replace += balloon.charAt(i);
+                S = S.replaceFirst(replace, "");
+
+                addChar = "";
+                addChar += balloon.charAt(i);
+                findBalloon += addChar;
+            }
+            else{
+                return noOfBalloons;
+            }
+
+            if(findBalloon.equals("BALLOON")) {
+                i = -1;
+                noOfBalloons ++;
+                findBalloon = "";
+            }
+        }
+
+        return noOfBalloons;
+    }
+}
