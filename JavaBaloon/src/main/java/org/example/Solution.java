294840e438283fb82cfea2b2326746a47a925d15
Dusan D Markovic <Dusan-D.Markovic@comtrade.com>
2022-09-20 10:47:16 +0200

    Added reading from a file and writing results to another file

--- JavaBaloon/src/main/java/org/example/Solution.java
+++ JavaBaloon/src/main/java/org/example/Solution.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
 
     public String wordToFind;
@@ -38,5 +42,62 @@ public class Solution {
         }
 
         return noOfBalloons;
+    }
+
+    public String[] readLines(String filePath){
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(filePath));
+            List<String> lines = new ArrayList<String>();
+            //String line = reader.readLine();
+            String line = reader.readLine();
+
+            while (line != null) {
+                lines.add(line);
+                line = reader.readLine();
+            }
+            reader.close();
+
+            return lines.toArray(new String[0]);
+        }
+        catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public void writeResults(String[] lines){
+
+        File file = null;
+
+        try {
+            file = new File("results.txt");
+            if (file.createNewFile())
+                System.out.println("File successfully created: " + file.getName());
+            else
+                System.out.println("File already exists.");
+        }
+        catch (IOException e) {
+            System.out.println("An error occurred while creating a file.");
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                FileWriter writer = new FileWriter(file);
+                for(String s : lines) {
+                    writer.write(Integer.toString(this.solution(s)));
+                    writer.write('\n');
+                }
+                writer.close();
+            }
+            catch (IOException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+        }
     }
 }
