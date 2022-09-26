b6aeefa091335e77f80b5e19c203497a474489d2
Dusan D Markovic <Dusan-D.Markovic@comtrade.com>
2022-09-26 16:34:47 +0200

    Added Maven assignment

---
+++ MavenParent/MavenChild1/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.slf4j.*;
+import com.google.common.base.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ MavenParent/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import org.slf4j.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
